5c1895234a2830ab25cc0686ef38d28908640bba
Sojournercntl <sojournercntl@users.noreply.github.com>
2019-06-24 00:24:19 +0200

    Added test project
    Added an empty test project for future unit tests

---
+++ ReactorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReactorTests
+{
+    [TestClass]
+    public class ReactorServerTest
+    {
+        [TestMethod]
+        public void TestCreation()
+        {
+
+        }
+    }
+}
